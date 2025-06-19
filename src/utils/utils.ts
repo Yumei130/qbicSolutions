4727d033b07fa682fa3fca6b4c12c5755d8ed19c
Daniela <daniela.garciac@sfa.michoacan.gob.mx>
2025-06-19 14:46:49 -0600

    Actualizacion deploy

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 const getImagePrefix = () => {
-  return process.env.NODE_ENV === "production" ? "/QbicSolutions/" : "";
+  // return process.env.NODE_ENV === "production" ? "/QbicSolutions/" : "";
+  return "";
 };
 
 export { getImagePrefix };
